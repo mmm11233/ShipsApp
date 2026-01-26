67c4890e8dd37e6e87549b3f5bf3454201d5fa5b
MariamJoglidze1 <mariam_joglidze@epam.com>
2026-01-26 23:07:42 +0400

    Initial Commit

---
+++ ShipsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShipsApp
+//
+//  Created by Mariam Joglidze on 26.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
