b2161fe10803b1466319401f68d43fae8a06bfbd
Mariam Joglidze <mariamjoglidze1@gmail.com>
2026-01-31 00:17:12 +0400

    Created ShipListView UI, made a custom ship model and viewModel to test the UI's work

---
+++ ShipsApp/Scenes/ListView/ShipsListView.swift
@@ -0,0 +1,85 @@
+//
+//  ShipsListView.swift
+//  ShipsApp
+//
+//  Created by Mariam Joglidze on 26.01.26.
+//
+
+import SwiftUI
+
+struct ShipsListView: View {
+    
+    @StateObject private var viewModel = ShipsViewModel()
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                
+                TextField("Search Ships", text: $viewModel.searchText)
+            }
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(viewModel.filteredShips) { ship in
+                        ShipCardView(ship: ship)
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct ShipCardView: View {
+    
+    let ship: Ship
+    
+    var body: some View {
+        
+        ZStack {
+            Image(systemName: "photo")
+                .resizable()
+                .foregroundColor(.gray)
+                .font(.title2)
+            
+            HStack{
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(ship.name)
+                        .font(.headline)
+                        .foregroundColor(.gray)
+
+                    Text(ship.type)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing, spacing: 58) {
+                    Image(systemName: ship.isFavorite ? "heart.fill" : "heart")
+                        .foregroundColor(ship.isFavorite ? .black : .gray)
+                    
+                    Text(ship.status)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+            .background(Color.black.opacity(0.25))
+        }
+        .frame(height: 150)
+        .cornerRadius(12)
+    }
+}
+
+
+#Preview {
+    ShipsListView()
+}
---
+++ ShipsApp/Scenes/ListView/ShipsListViewViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ShipsListViewViewModel.swift
+//  ShipsApp
+//
+//  Created by Mariam Joglidze on 30.01.26.
+//
+
+import SwiftUI
+import Foundation
+import Combine
+
+final class ShipsViewModel: ObservableObject {
+    @Published var searchText: String = ""
+    
+    @Published var ships: [Ship] = [
+        Ship(name: "MR STEVEN", type: "Tug", status: "Stopped", isFavorite: true),
+        Ship(name: "Falcon 9", type: "Ship", status: "Active", isFavorite: false)
+    ]
+    
+    var filteredShips: [Ship] {
+        guard !searchText.isEmpty else { return ships }
+        return ships.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText)
+        }
+    }
+}
+
+struct Ship: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let status: String
+    let isFavorite: Bool
+}
+
+
