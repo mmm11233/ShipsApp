4acf48b6327952fd607f285e2ba4e0bac25b29ff
Mariam Joglidze <mariamjoglidze1@gmail.com>
2026-01-31 18:31:52 +0400

    changed the names of ListView and ListViewModel. Created  files of ShipDetail and ShipDetailViewModel

---
+++ ShipsApp/Scenes/ShipDetail/ShipDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ShipDetailViewModel.swift
+//  ShipsApp
+//
+//  Created by Mariam Joglidze on 31.01.26.
+//
+
+import Foundation
+import SwiftUI
+
+final class ShipDetailViewModel {
+    
+    init() {
+      
+    }
+}
